6a4f7ea32f5b422137176c1f92554efb76f6bb5e
Ikjot Singh <ikjotmohali@gmail.com>
2026-07-23 13:06:08 +0530

    More on array

--- javachitkara/test.java
+++ javachitkara/test.java
@@ -1,6 +1,12 @@
 public class test {
 
     public static void main(String[] args){
-   
+   int[] number = new int[5];
+   for(int i=0;i<3;i++){
+    number[i] = i+1;
+   }
+   for(int i=0;i<5;i++){
+    System.out.println(number[i]);
+   }
     }
 }
